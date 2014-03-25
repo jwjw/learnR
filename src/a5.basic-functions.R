34a889673fee34a805d41c382aa33cd0670398ef
Kun Ren <renkun@outlook.com>
2014-03-26 00:11:56 +0800

    a3 updated

--- src/a5.basic-functions.R
+++ src/a5.basic-functions.R
@@ -1,5 +1,15 @@
 # Basic functions
 
+## Object functions
+
+## Character functions
+cat("Hello, world!")
+message("Hello, world!")
+print("Hello, world!")
+cat("A new line\nA tab\t")
+message("A new line\nA tab\t")
+sprintf("Hello, %s! Your number is %d., and your score is %.2f","Jack",50,35.618)
+
 ## Data manipulation
 
 ### Data read/write
