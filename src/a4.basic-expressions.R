08a8d0df1aefcdfdae73b3754657e9ae8b08668f
Kun Ren <renkun@outlook.com>
2014-03-26 14:37:37 +0800

    minor update

--- src/a4.basic-expressions.R
+++ src/a4.basic-expressions.R
@@ -2,7 +2,27 @@
 
 ## Conditional expression
 
+### if-else
+
+num <- as.numeric(readline("Number? "))
+if(num > 0) {
+  cat("The number is greater than 0")
+} else if(num < 0) {
+  cat("The number is less than 0")
+} else {
+  cat("The number is 0")
+}
+
+### ifelse
+
+num1 <- as.numeric(readline("Number 1? "))
+num2 <- as.numeric(readline("Number 2? "))
+num3 <- ifelse(num1 > 0 & num2 < 0, 1, 0)
+
 ## Loop expression
 
+
+
 ## Output expression
+
 
