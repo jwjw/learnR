ef40fb78f7ed8cc9b5123dbdf08d814a2fc006e3
Kun Ren <renkun@outlook.com>
2014-03-31 13:32:33 +0800

    a5 updated

--- src/a5.basic-functions.R
+++ src/a5.basic-functions.R
@@ -73,9 +73,46 @@ substr(ch1,1,3)
 substr(ch1,1,3) <- "abc"
 
 ## Math funcitons
+sin(1)
+cos(1)
+tan(1)
+exp(-1)
+sinh(1)
+cosh(1)
+tanh(1)
+factorial(5)
+ceiling(10.6)
+floor(9.5)
+trunc(1.5)
+round(pi,3)
+signif(pi,3)
+
+
+polyroot(c(1,2,1,-1))
+f <- deriv(y~sin(cos(x)*y),c("x","y"),func=T)
+f(1,2)
 
 
 ## Statistical functions
+x.norm <- rnorm(20)
+mean(x.norm)
+sd(x.norm)
+median(x.norm)
+quantile(rnorm(x.norm))
+max(x.norm)
+min(x.norm)
+pmax(c(1,2,3),c(2,3,1))
+pmin(c(1,2,3),c(2,3,1),c(3,2,1))
+summary(x.norm)
+
+set.seed(100)
+sample(c(1,2,3),size = 2,replace = FALSE)
+sample(c("a","b","z"),size = 10, replace = TRUE)
+rnorm(20,mean = 10,sd = 2)
+rbinom(20,size = 10,prob = 0.6)
+runif(20)
+runif(20,min = 10,max = 20)
+rgamma(20,shape = 0.5)
 
 
 ## Data manipulation
@@ -84,7 +121,7 @@ substr(ch1,1,3) <- "abc"
 EURUSD5 <- read.csv("data/EURUSD5.csv",header=FALSE,
   col.names=c("Date","Time","Open","High","Low","Close","Volume"))
 
-sh000300 <- read.csv("data/sh000300.csv",header = T,
+idx <- read.csv("data/sh000300.csv",header = T,
   col.names=c("date","open","high","low","close","volume","tradeVolume"),
   colClasses=c("Date","numeric","numeric","numeric","numeric","numeric","numeric"))
 us.cpi <- read.csv("data/us-cpi.csv",header = T,
@@ -105,6 +142,22 @@ EURUSD5a <- transform(EURUSD5, Medium=(High+Low)/2)
 write.csv(EURUSD5a,"output/EURUSD5a.local.csv",row.names=FALSE)
 write.table(EURUSD5a,"output/EURUSD5a.local.txt",row.names=FALSE,sep="\t")
 
+#### Read/Write via RSQLite
+##### Write data.frame to SQLite database
+require(RSQLite)
+conn <- dbConnect(SQLite(),"data/example.local.sqlite")
+n <- 1000000
+products.df <- data.frame(i=1:n,type=sample(LETTERS,n,T),
+  class=sample(LETTERS[1:3],n,T),x=rnorm(n,6,2),y=rbinom(n,10,0.6))
+dbWriteTable(conn,"products",products.df)
+dbDisconnect(conn)
+
+##### Read data.frame from SQLite database
+require(RSQLite)
+conn <- dbConnect(SQLite(),"data/example.local.sqlite")
+products.A <- dbGetQuery(conn,"SELECT * FROM products WHERE type='A'")
+products.subset <- dbGetQuery(conn,"SELECT i,class,x+y AS z FROM products WHERE type='A' AND x>=8")
+dbDisconnect(conn)
 
 ## Higher-order functions
 
