fe48951fee1772a332c22914c28682c7c1d5f8fb
Kun Ren <renkun@outlook.com>
2014-04-02 17:01:55 +0800

    a9 updated

--- src/a9.design-patterns.R
+++ src/a9.design-patterns.R
@@ -1,6 +1,33 @@
 # Design patterns
+# http://renkun.me/blog/r/2014/02/15/a-principle-of-writing-robust-r-program.html
 
-## Independence of Logic-Data-Interface
+## data
 
+## input
+profile.default <- list(
+  name = "test1",
+  n = 20,
+  random = c("rnorm","runif","rnorm"),
+  range = c(0.2,0.8),
+  columns = c("a","b","c")
+)
 
-## Basic functional programming
+profile.user <- list(
+  n = 30
+)
+
+profile <- modifyList(profile.default,profile.user)
+
+## logic
+data <- lapply(profile$random,function(fun) {
+  rnd <- get(fun)
+  val <- rnd(n=profile$n)
+  val[val < profile$range[1]] <- profile$range[1]
+  val[val > profile$range[2]] <- profile$range[2]
+  return(val)
+})
+df <- data.frame(do.call(cbind,data))
+colnames(df) <- profile$columns
+
+## output
+df
