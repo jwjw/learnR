a4a1acecc9747345deebcfecd3de0dcd21eba513
Kun Ren <renkun@outlook.com>
2014-04-03 21:50:01 +0800

    b4 updated

--- src/b4.parallel-computing.R
+++ src/b4.parallel-computing.R
@@ -6,7 +6,7 @@ cl <- makeCluster(detectCores())
 clusterCall(cl, function() {
   n <<- 100
 })
-coes <- as.numeric(clusterApply(cl,1:1000,function(i) {
+coes <- as.numeric(parLapply(cl,1:10000,function(i) {
   x <- rnorm(n)
   y <- 2*x+rnorm(n)*0.1
   m <- lm(y~x)
@@ -20,7 +20,48 @@ hist(coes)
 ## parallelMap
 library(parallelMap)
 parallelStart(mode="socket",cpus=4)
-
+result <- parallelSapply(1:10000, function(i,n) {
+  x <- rnorm(n)
+  y <- 2*x+rnorm(n)*0.1
+  m <- lm(y~x)
+  coe <- coef(m)
+  coe[[2]]
+},n=100)
 parallelStop()
 
+
+## foreach
+library(foreach)
+library(doParallel)
+cl <- makeCluster(detectCores())
+clusterCall(cl, function() {
+  n <<- 100
+})
+registerDoParallel(cl)
+result <- foreach(i=1:10000) %dopar% {
+  x <- rnorm(n)
+  y <- 2*x+rnorm(n)*0.1
+  m <- lm(y~x)
+  coe <- coef(m)
+  coe[[2]]
+}
+stopCluster(cl)
+
+
 ## doParallel + plyr
+library(plyr)
+library(doParallel)
+cl <- makeCluster(detectCores())
+clusterCall(cl, function() {
+  n <<- 100
+})
+registerDoParallel(cl)
+result <- llply(1:10000,function(i) {
+  x <- rnorm(n)
+  y <- 2*x+rnorm(n)*0.1
+  m <- lm(y~x)
+  coe <- coef(m)
+  coe[[2]]
+},.parallel=T)
+stopCluster(cl)
+
