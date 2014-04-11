aa62fd3d37964571bc853b2b6df694db3d87d0bf
Kun Ren <renkun@outlook.com>
2014-04-11 12:34:52 +0800

    c5 updated

--- src/c5.plyr.R
+++ src/c5.plyr.R
@@ -2,7 +2,6 @@
 library(plyr)
 l1 <- list(rnorm(5),rnorm(10),rnorm(30))
 
-
 ## llply
 llply(l1,function(item) {
   c(mean=mean(item),sum=sum(item))
@@ -13,17 +12,21 @@ ldply(l1,function(item) {
   c(n=length(item),mean=mean(item),sum=sum(item))
 })
 
+ldply(l1,function(item) {
+  data.frame(n=length(item),mean=mean(item),sum=sum(item))
+})
+
 n <- 500000
-d1 <- data.frame(type=sample(LETTERS[1:6],n,T),subtype=sample(letters[1:3],n,T),
-  x=rnorm(n),y=rbinom(n,10,0.6),z=rgamma(n,0.5))
+d1 <- data.frame(type=sample(LETTERS[1:6],n,T),subtype=sample(letters[1:3],n,T), x=rnorm(n),y=rbinom(n,10,0.6),z=rgamma(n,0.5))
 
 ## dlply
-dlply(d1,c("type","subtype"),function(df) {
+ms <- dlply(d1,c("type","subtype"),function(df) {
   lm(z~x+y,data=df)
 })
 
 ## ddply
-ddply(d1,c("type","subtype"),function(df) {
+ms.coes <- ddply(d1,c("type","subtype"),function(df) {
   m <- lm(z~x+y,data=df)
-  coef(m)
+  info <- summary(m)
+  c(coef(m),rsq=info$r.squared)
 })
