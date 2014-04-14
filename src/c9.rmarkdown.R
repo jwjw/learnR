3bf0389c90468c3120bbe81d21db75bb6ffd1e7f
Kun Ren <renkun@outlook.com>
2014-04-14 15:25:39 +0800

    add c9: rmarkdown

---
+++ src/c9.rmarkdown.R
@@ -0,0 +1,12 @@
+# R Markdown
+
+## pandoc
+# http://johnmacfarlane.net/pandoc/
+
+## rmarkdown
+# http://rmarkdown.rstudio.com/
+### Markdown basics (http://rmarkdown.rstudio.com/authoring_basics.html)
+### R code chunks (http://rmarkdown.rstudio.com/authoring_rcodechunks.html)
+### Pandoc markdown (http://rmarkdown.rstudio.com/authoring_pandoc_markdown.html)
+### PDF documents (http://rmarkdown.rstudio.com/pdf_document_format.html)
+### Markdown document format (http://rmarkdown.rstudio.com/markdown_document_format.html)
