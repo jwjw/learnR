bec8e2de2da7cb60280880e1a696aa03f8a1ec70
Kun Ren <renkun@outlook.com>
2014-04-14 09:23:54 +0800

    c3 updated

--- src/c3.stringr.R
+++ src/c3.stringr.R
@@ -1,6 +1,33 @@
 # Regular expressions
 # http://regexone.com/lesson/0
 
+# abc…    Letters
+# 123…    Digits
+# \d  any Digit
+# .   any Character
+# \.  Period
+# [abc]   Only a, b, or c
+# [^abc]  Not a, b, nor c
+# [a-z]   Characters a to z
+# [0-9]   Numbers 0 to 9
+# {m}     m Repetitions
+# {m,n}   m to n Repetitions
+# *   Zero or more repetitions
+# +   One or more repetitions
+# ?   Optional
+# \s  any Whitespace
+# ^…$     Starts and ends
+# ()  capture Group
+# (a(bc))     capture Sub group
+# (.*)    capture Variable content
+# (a|b)   Matchs a or b
+# \w  any Alphanumeric character
+# \W  any Non-alphanumeric character
+# \d  any Digit
+# \D  any Non-digit character
+# \s  any Whitespace
+# \S  any Non-whitespace character
+
 # {stringr} package
 library(stringr)
 
